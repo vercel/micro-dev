90c74943debf7a825202b8c66bf4e409c45a68d8
Leo Lamprecht <mindrun@icloud.com>
2017-07-27 20:20:57 +0200

    Now able to run microservices

--- bin/micro-dev.js
+++ bin/micro-dev.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 
+// Native
+const { existsSync } = require('fs')
+const path = require('path')
+
 // Packages
 const mri = require('mri')
 
 // Utilities
 const showHelp = require('../lib/help')
 const {version} = require('../package')
+const serve = require('../lib/serve')
 
 const flags = mri(process.argv.slice(2), {
   boolean: [
@@ -31,3 +36,34 @@ if (flags.version) {
 if (flags.help) {
   showHelp()
 }
+
+let file = flags._[0]
+
+if (!file) {
+  try {
+    // eslint-disable-next-line import/no-dynamic-require
+    const packageJson = require(path.resolve(process.cwd(), 'package.json'))
+    file = packageJson.main || 'index.js'
+  } catch (err) {
+    if (err.code !== 'MODULE_NOT_FOUND') {
+      console.error(`Could not read \`package.json\`: ${err.message}`)
+      process.exit(1)
+    }
+  }
+}
+
+if (!file) {
+  console.error('Please supply a file!')
+  process.exit(1)
+}
+
+if (file[0] !== '/') {
+  file = path.resolve(process.cwd(), file)
+}
+
+if (!existsSync(file)) {
+  console.log(`The file or directory "${path.basename(file)}" doesn't exist!`)
+  process.exit(1)
+}
+
+serve(file, flags)
