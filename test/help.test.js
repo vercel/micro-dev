93a83dbb71a431b4e5214607bc32be69b7d0b00d
Kim Hallberg <hallberg.kim@gmail.com>
2023-01-02 20:45:11 +0100

    Add `micro-dev` as dev dependency

--- test/help.test.js
+++ test/help.test.js
@@ -1,5 +1,5 @@
 import { expect, it } from 'vitest'
-import generateHelp from '../packages/micro-dev/src/lib/help'
+import generateHelp from 'micro-dev/src/lib/help'
 
 it('--help', () => {
   const result = generateHelp()
